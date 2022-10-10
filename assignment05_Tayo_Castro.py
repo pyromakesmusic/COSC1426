d9a6433ddb1e77f9fb70e235186b1752fa7c9394
pyromakesmusic <pyromakesmusic@gmail.com>
2022-10-10 15:04:23 -0500

    put together half the file

---
+++ assignment05_Tayo_Castro.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Tayo Castro
+COSC 1426 TTh
+Assignment 05
+"""
+def running_total(iterations):
+    most_expensivest = "NA"
+    for i in range(interations):
+        item_name = input("Item name: ")
+        item_price = float(input("Item price: "))
+
+def main():
+    print("Welcome!")
+    expenditures = int(input("How many expenditures?"))
+    budget = float(input("What was your budget last month?"))
+    most_expensivest = "NA"
+    for i in range(expenditures):
+        item_name = input("Item name: ")
+        item_price = float(input("Item price: "))
+
+main()
