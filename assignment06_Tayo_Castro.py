32bb283da01af81345dc253a1771c4fcb6cff5e6
pyromakesmusic <tayo.castro@gmail.com>
2022-10-21 01:00:24 -0500

    added some new files

--- assignment06_Tayo_Castro.py
+++ assignment06_Tayo_Castro.py
@@ -23,36 +23,75 @@ def init():
     """
     print_input = input_to_flag(input("Would you like to print X, Y, or Zee? [yes/no] "))
     return print_input
+
+def face_from_name(face):
+    if face == "cute":
+        print("^.^", end="")
+    elif face == "meh":
+        print(".-.", end="")
+    elif face == "wow":
+        print("@~@", end="")
+    elif face == "neutral":
+        print("*_*", end="")
+
+def make_face(face_name, size):
+    for col in range(size):
+        print(" ", end="")
+    face_from_name(face_name)
+    for col in range(size):
+        print(" ", end ="")
+    print()
+    return
+
 def top_triangle(size, symbol):
-    for row in range(0, size + 1, 2):
-        for x in range(row):
+    for row in range(size):
+        for col in range((2 * row) - 1):
             print(" ", end="")
-        for col in range(size - row):
-            print("* ", end = "")
+        print(f"{symbol}", end="")
+        for col in range((3 * size) - (3 * row)):
+            print(" ", end="")
+        print(f"{symbol}", end="")
         print()
     return
 
 def bottom_triangle(size, symbol):
+    for row in range(size):
+        for col in range(((2 * size) - 1) - (2 * row)):
+            print(" ", end="")
+        print(f"{symbol}", end="")
+        for col in range((2 * row) - 1):
+            print("  ", end="")
+        print(f"{symbol}", end="")
+        print()
     return
 
 def leg(size, symbol):
+    for row in range(size):
+        for col in range((2 * size) - 1):
+            print(" ", end="")
+        print(f"{symbol}")
+        print()
     return
 
 def print_x(size, symbol="o"):
-    for row in range(0, ((2 * size) + 1)):
-        print(symbol, end="")
-        for col in range(row, size + 2):
-            print(" ", end="")
-        print()
-        print(symbol)
+    top_triangle(size, symbol)
+    bottom_triangle(size,symbol)
 
     return
 
-def print_y(size):
+def print_y(size, symbol):
+    top_triangle(size, symbol)
+    leg(size, symbol)
     return
 
-def print_face(face):
-    return
+def print_zee(face, size):
+    make_face(face, size)
+    top_triangle(size, "o")
+    bottom_triangle(size, "o")
+
+def print_chevron(size, symbol):
+    top_triangle(size, symbol)
+    top_triangle(size, symbol)
 
 def print_loop(letter_or_character, letter, face, size, symbol=""):
     """
@@ -64,9 +103,14 @@ def print_loop(letter_or_character, letter, face, size, symbol=""):
     """
     print(f"Printing {letter_or_character} of size {size}")
     if letter_or_character == "letter":
-        print_x(size, symbol)
+        if letter == "x":
+            print_x(size, symbol)
+        elif letter == "y":
+            print_y(size, symbol)
     elif letter_or_character == "character":
-        pass
+        print_zee(face, size)
+    elif letter_or_character == "chevron":
+        print_chevron(size,symbol)
     return
 
 def char_print():
@@ -74,12 +118,12 @@ def char_print():
     This contains most of the logic of getting user input and converting it into the form needed for the print function
     :return:
     """
-    l_or_c = input("What would you like to print? [letter/character] ")
+    l_or_c = input("What would you like to print? [letter/character/chevron] ")
     symbol = ""
     letter = ""
     face = ""
 
-    while l_or_c != "letter" and l_or_c != "character":
+    while l_or_c != "letter" and l_or_c != "character" and l_or_c != "chevron":
         l_or_c = input("Sorry, I can't print that, try again: ")
     if l_or_c == "letter":
         letter = input("What letter? [x/y] ")
@@ -92,6 +136,10 @@ def char_print():
         face = input("What face would you like to use? [cute/meh/wow/neutral]")
         while face != "cute" and face != "meh" and face != "wow" and face!= "neutral":
             face = input("That isn't a valid face, try again: ")
+    elif l_or_c == "chevron":
+        symbol = input("What symbol would you like to use? ")
+        while symbol == "":
+            symbol = input("Oops, you didn't enter anything, try again: ")
 
     size = random.randint(1,5)
 
@@ -104,13 +152,20 @@ def main():
         char_print()
         print_flag = input_to_flag(input("Print again? [yes/no] "))
 
-# main()
+main()
 
-top_triangle(9, "0")
+
 
 """
 DESIGN DOC QUESTIONS
 ====================
-
+● How long did you spend on this assignment?
+Maybe 8-10 hours, and then 2 minutes on the extension.
+● Is there anything you would change/improve about your assignment?
+The algorithms for the whitespace characters are incorrect.
+● How did you figure out how to print the letters?
+I broke the letters up into pieces; they share some parts.
+● Explain the process of putting Zee together.
+It was an X, plus a face input.
 
 """
---
+++ assignment07_Tayo_Castro.py
@@ -0,0 +1,37 @@
+# Tayo Castro
+# COSC 1423 TTh
+# Assignment 07
+
+
+
+def split_file():
+    filename = input("Name of file to split: ")
+    try:
+        file_obj = open(filename, "r")
+    except FileNotFoundError:
+        filename = input("Whoops! File doesn't exist. Try again: ")
+        file_obj = open(filename, "r")
+	pass
+
+# See write up for implementation details
+def append_files():
+    filename = input("Name of file to write into: ")
+    file_obj = open(filename, "w")
+	pass
+
+
+def main():
+    return
+
+main()
+
+"""
+DESIGN DOC QUESTIONS
+====================
+● How long did you spend on the assignment?
+● Did you do the extension?
+● What would you have changed given more time/knowledge?
+● Explain your splitting implementation.
+● Explain your appending implementation.
+
+"""
