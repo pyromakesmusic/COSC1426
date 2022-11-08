949d04a1a661f4958d2f119b0cb0dbbafcc11057
pyromakesmusic <pyromakesmusic@gmail.com>
2022-11-07 23:25:02 -0600

    did most of assignment 08

--- assignment08_Tayo_Castro.py
+++ assignment08_Tayo_Castro.py
@@ -6,34 +6,55 @@ SLOT = "_"
 MONSTER1 = "X"
 MONSTER2= "O"
 
+def pretty_print_board(board):
+    for row in board:
+        for column in row:
+            print(column, end=" ")
+        print()
+    return
+
 def win_condition_checker(board):
     win = None
-    for i in range(3):
-        for j in range(3):
-            if board[i][j] == board[i][j] == board[i][j]:
-                print("You Won!")
-                win = True
-            else:
-                win = False
+    print(type(board))
+    wins_horizontally = board[0][0] == board[0][1] == board[0][2] or board[1][0] == board[1][1] == board[1][2] or \
+                        board[2][0] == board[2][1] == board[2][2]
+
+    wins_vertically = board[0][0] == board[1][0] == board[2][0] or board[0][1] == board[1][1] == board[2][1] or \
+                        board[0][2] == board[1][2] == board[2][2]
+
+    wins_diagonally = board[0][0] == board[1][1] == board[2][2] or board[2][0] == board[1][1] == board[0][2]
+
+    if wins_horizontally:
+        win = "horizontally"
+    elif wins_vertically:
+        win = "vertically"
+    elif wins_diagonally:
+        win = "diagonally"
+    else:
+        win = False
     return win
 
 def row_and_column_getter():
-    row = int(input("Row:"))
+    row = int(input("Row: "))
     while row < 0 or row > 2:
         row = int(input("Oops, out of range, try again: "))
-    column = int(input("Column:"))
+    column = int(input("Column: "))
     while column < 0 or column > 2:
         column = int(input("Oops, out of range, try again: "))
+
+    print(row, column)
     return row, column
 
 def place_checker_and_player(board, parity):
+    print("Player", parity + 1, "Turn: ")
     row, column = row_and_column_getter()
     while board[row][column] != SLOT:
         print("Sorry, that space is taken! Try again.")
         row, column = row_and_column_getter()
 
 
-    if win_condition_checker(board):
+    if win_condition_checker(board) == "horizontally" or win_condition_checker(board) == "vertically" \
+            or win_condition_checker(board) == "diagonally":
         print("Player", parity + 1, "Won!")
         return
     else:
@@ -41,7 +62,7 @@ def place_checker_and_player(board, parity):
             board[row][column] = MONSTER1
         else:
             board[row][column] = MONSTER2
-        return
+        return board
 
 
 def game_loop():
@@ -50,11 +71,18 @@ def game_loop():
         [SLOT,SLOT,SLOT],
         [SLOT,SLOT,SLOT]
     ]
+    print(type(board[0][1]))
     win = False
     parity = 0
 
-    while win = False:
-        place_checker_and_player(board, parity)
+    while win == False:
+        print("Board: ")
+        print(board)
+        board = place_checker_and_player(board, parity)
+        print("Board:")
+        print(board)
+        #pretty_print_board(board)
+        win = win_condition_checker(board)
         parity = (parity + 1) % 2
 
     return
